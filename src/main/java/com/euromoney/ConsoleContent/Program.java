ad499a079b61c1640498d32e55d835e6666d3ddd
Howell, Andy (UK) <ahowell@euromoneyplc.com>
2015-04-16 14:36:05 +0100

    Initial commit

---
+++ src/main/java/com/euromoney/ConsoleContent/Program.java
@@ -0,0 +1,17 @@
+package com.euromoney.ConsoleContent;
+
+import java.io.IOException;
+
+public class Program {
+
+	/**
+	 * Initialises the application in the
+	 * console.
+	 * 
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+
+	}
+
+}
